0b2c9669c6188e5ed292fb831f413749b120e81a
Luke Reed <luke.reed@outlook.com.au>
2023-08-14 08:14:04 +1000

    Edited sportsbet script: split afl and nrl into two functions as element class names are different

--- controllers/sportsbetController.js
+++ controllers/sportsbetController.js
@@ -1,8 +1,8 @@
-const { sportsbet } = require("../scripts/sportsbet");
+const { sportsbetAFL, sportsbetNRL } = require("../scripts/sportsbet");
 
 const rugbyLeague = async (req, res) => {
 	try {
-		const gameData = await sportsbet("rugby-league/nrl");
+		const gameData = await sportsbetNRL();
 		res.status(200).json(gameData);
 	} catch (error) {
 		res.status(500).json({ error });
@@ -11,7 +11,7 @@ const rugbyLeague = async (req, res) => {
 
 const afl = async (req, res) => {
 	try {
-		const gameData = await sportsbet("australian-rules/afl");
+		const gameData = await sportsbetAFL();
 		res.status(200).json(gameData);
 	} catch (error) {
 		res.status(500).json({ message: error });

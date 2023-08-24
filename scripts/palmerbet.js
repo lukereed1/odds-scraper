11fc114a5169ac171e03c243ee2f3b6132aed868
Luke Reed <luke.reed@outlook.com.au>
2023-08-24 15:43:14 +1000

    Added palmerbet script

---
+++ scripts/palmerbet.js
@@ -0,0 +1,41 @@
+const puppeteer = require("puppeteer-extra");
+const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+puppeteer.use(StealthPlugin());
+
+/*--------------------------------------------------------------------*/
+/*------------------------Rugby League, AFL---------------------------*/
+/*--------------------------------------------------------------------*/
+async function palmerbet(sportURL) {
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+	await page.goto(`${sportURL}`, {
+		waitUntil: "networkidle0",
+	});
+
+	const teamAndOdds = await page.evaluate(() => {
+		const gamesList = [];
+		// All game cards
+		const gameCards = document.querySelectorAll(".detail-container");
+
+		gameCards.forEach((game) => {
+			// All team names and odds within each card
+			const teams = game.querySelectorAll(".team-info.team-info--has-logo");
+			const odds = game.querySelectorAll(".ng-star-inserted.alt-button-design");
+
+			const gamesData = {
+				firstTeam: teams[0].innerText,
+				secondTeam: teams[1].innerText,
+				firstTeamOdds: odds[0].innerText,
+				secondeTeamOdds: odds[1].innerText,
+			};
+
+			gamesList.push(gamesData);
+		});
+
+		return gamesList;
+	});
+
+	return teamAndOdds;
+}
+
+module.exports = { palmerbet };

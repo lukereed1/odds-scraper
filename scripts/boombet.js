f04d7739fb57ec7b94623292641cefd9959be0b7
Luke Reed <luke.reed@outlook.com.au>
2023-08-27 14:36:42 +1000

    Boombet soccer implemented

--- scripts/boombet.js
+++ scripts/boombet.js
@@ -39,4 +39,42 @@ async function boombet(sport) {
 	return teamAndOdds;
 }
 
-module.exports = { boombet };
+async function boombetSoccer(league) {
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+	page.setDefaultTimeout(120000);
+	await page.goto(`https://boombet.com.au/sport-menu/Sport/Soccer/${league}`, {
+		waitUntil: "networkidle0",
+	});
+
+	const teamAndOdds = await page.evaluate(() => {
+		const gamesList = [];
+
+		// All game cards
+		const gameCards = document.querySelectorAll(".listItemWrapper");
+
+		gameCards.forEach((game) => {
+			// All team names and odds within each card
+			const teams = game.querySelectorAll(".teamName.d-block.d-md-flex.pb-1");
+			const odds = game.querySelectorAll(".oddsValue");
+
+			const gamesData = {
+				bookie: "BoomBet",
+				firstTeam: teams[0].innerText,
+				secondTeam: teams[1].innerText,
+				firstTeamOdds: odds[0].innerText,
+				drawOdds: odds[1].innerText,
+				secondTeamOdds: odds[2].innerText,
+			};
+
+			gamesList.push(gamesData);
+		});
+
+		return gamesList;
+	});
+
+	await browser.close();
+	return teamAndOdds;
+}
+
+module.exports = { boombet, boombetSoccer };
--- scripts/neds.js
+++ scripts/neds.js
@@ -42,4 +42,47 @@ async function neds(sport) {
 	return teamAndOdds;
 }
 
-module.exports = { neds };
+async function nedsSoccer(league) {
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+	page.setDefaultTimeout(120000);
+	await page.goto(`https://www.neds.com.au/sports/soccer/${league}`, {
+		waitUntil: "networkidle2",
+	});
+
+	const teamAndOdds = await page.evaluate(() => {
+		const gamesList = [];
+
+		// All game cards
+		const gameCards = document.querySelectorAll(
+			".sports-market-primary__prices-inner"
+		);
+
+		gameCards.forEach((game) => {
+			// All team names and odds within each card
+			const teams = game.querySelectorAll(".displayTitle");
+
+			const odds = game.querySelectorAll(".price-button-odds-price");
+
+			const gamesData = {
+				bookie: "Neds",
+				firstTeam: teams[0].innerText,
+				secondTeam: teams[1].innerText,
+				firstTeamOdds: odds[0].innerText,
+				drawOdds: odds[1].innerText,
+				secondTeamOdds: odds[2].innerText,
+			};
+
+			gamesList.push(gamesData);
+		});
+
+		return gamesList;
+	});
+
+	await browser.close();
+	console.log(teamAndOdds);
+}
+
+nedsSoccer("uk-ireland/premier-league");
+
+module.exports = { neds, nedsSoccer };

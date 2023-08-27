9c58e5b9f449c9a2cbe25a0b8c40e0ff87a3240a
Luke Reed <luke.reed@outlook.com.au>
2023-08-27 15:25:54 +1000

    Soccer scraper complete for soccer

--- scripts/unibet.js
+++ scripts/unibet.js
@@ -46,6 +46,52 @@ async function unibet(sport) {
 	return teamAndOdds;
 }
 
+async function unibetSoccer(league) {
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+	page.setDefaultTimeout(120000);
+	await page.goto(
+		`https://www.unibet.com.au/betting/sports/filter/football/${league}/all/matches`,
+		{
+			waitUntil: "networkidle0",
+		}
+	);
+
+	const teamAndOdds = await page.evaluate(() => {
+		const gamesList = [];
+
+		// All game cards
+		const gameCards = document.querySelectorAll(".c21a2");
+
+		gameCards.forEach((game) => {
+			// If game is live skip it without scraping its data
+			if (game.querySelector("._026a0._868eb.fe33e")) return;
+
+			// All team names and odds within each card
+			const teams = game.querySelectorAll(".c539a");
+
+			const odds = game.querySelectorAll("._8e013");
+
+			const gamesData = {
+				bookie: "Unibet",
+				firstTeam: teams[0].innerText,
+				secondTeam: teams[1].innerText,
+				firstTeamOdds: odds[0].innerText,
+				drawOdds: odds[1].innerText,
+				secondTeamOdds: odds[2].innerText,
+			};
+
+			gamesList.push(gamesData);
+		});
+
+		return gamesList;
+	});
+
+	await browser.close();
+	return teamAndOdds;
+}
+
 module.exports = {
 	unibet,
+	unibetSoccer,
 };

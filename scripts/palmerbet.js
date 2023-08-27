8e12a7f4d764d01d6c838582518d262c3ef12fd5
Luke Reed <luke.reed@outlook.com.au>
2023-08-27 14:52:52 +1000

    Soccer scraper complete for palmerbet

--- scripts/palmerbet.js
+++ scripts/palmerbet.js
@@ -19,6 +19,7 @@ async function palmerbet(sportURL) {
 		gameCards.forEach((game) => {
 			// All team names and odds within each card
 			const teams = game.querySelectorAll(".team-info.team-info--has-logo");
+
 			const odds = game.querySelectorAll(".ng-star-inserted.alt-button-design");
 
 			const gamesData = {
@@ -39,4 +40,45 @@ async function palmerbet(sportURL) {
 	return teamAndOdds;
 }
 
-module.exports = { palmerbet };
+async function palmerbetSoccer(sportURL) {
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+	page.setDefaultTimeout(120000);
+	await page.goto(`${sportURL}`, {
+		waitUntil: "networkidle0",
+	});
+
+	const teamAndOdds = await page.evaluate(() => {
+		const gamesList = [];
+
+		// All game cards
+		const gameCards = document.querySelectorAll(".detail-container");
+
+		gameCards.forEach((game) => {
+			// All team names and odds within each card
+			const teams = game.querySelectorAll(".team-info.team-info--has-logo");
+
+			const odds = game.querySelectorAll(
+				".additional-button-design.ng-star-inserted"
+			);
+
+			const gamesData = {
+				bookie: "PalmerBet",
+				firstTeam: teams[0].innerText,
+				secondTeam: teams[1].innerText,
+				firstTeamOdds: odds[0].innerText.split("\n")[1],
+				drawOdds: odds[2].innerText.split("\n")[1],
+				secondTeamOdds: odds[1].innerText.split("\n")[1],
+			};
+
+			gamesList.push(gamesData);
+		});
+
+		return gamesList;
+	});
+
+	await browser.close();
+	return teamAndOdds;
+}
+
+module.exports = { palmerbet, palmerbetSoccer };

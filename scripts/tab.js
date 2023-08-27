d8bc6b5d4f45b123a2dce06da79687c353591636
Luke Reed <luke.reed@outlook.com.au>
2023-08-27 15:10:46 +1000

    Completed soccer scraper for tab

--- scripts/tab.js
+++ scripts/tab.js
@@ -49,4 +49,50 @@ async function tab(sport) {
 	return teamAndOdds;
 }
 
-module.exports = { tab };
+async function tabSoccer(league) {
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+	page.setDefaultTimeout(120000);
+	await page.goto(
+		`https://www.tab.com.au/sports/betting/Soccer/competitions/${league}`,
+		{
+			waitUntil: "networkidle2",
+		}
+	);
+
+	const teamAndOdds = await page.evaluate(() => {
+		const gamesList = [];
+		// All game cards
+		const gameCards = document.querySelectorAll(".template-item");
+
+		gameCards.forEach((game) => {
+			// If game is live skip it without scraping its data
+			if (game.querySelector(".in-play")) return;
+
+			// All team names and odds within each card
+			const teams = game.querySelectorAll(".match-name-text");
+
+			const odds = game.querySelectorAll(".animate-odd");
+
+			const [firstTeam, secondTeam] = teams[0].innerText.split(" v ");
+
+			const gamesData = {
+				bookie: "Tab",
+				firstTeam: firstTeam,
+				secondTeam: secondTeam,
+				firstTeamOdds: odds[0].innerText,
+				drawOdds: odds[1].innerText,
+				secondTeamOdds: odds[2].innerText,
+			};
+
+			gamesList.push(gamesData);
+		});
+
+		return gamesList;
+	});
+
+	await browser.close();
+	return teamAndOdds;
+}
+
+module.exports = { tab, tabSoccer };

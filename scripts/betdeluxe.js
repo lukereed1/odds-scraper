1e3e451109dcb9b05283fb0eb144b91d4d314a7a
Luke Reed <luke.reed@outlook.com.au>
2023-08-27 14:28:17 +1000

    Betdeluxe soccer scraper complete

--- scripts/betdeluxe.js
+++ scripts/betdeluxe.js
@@ -50,4 +50,49 @@ async function betdeluxe(sportURL) {
 	return teamAndOdds;
 }
 
-module.exports = { betdeluxe };
+async function betdeluxeSoccer(league) {
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+	page.setDefaultTimeout(120000);
+	await page.goto(`https://betdeluxe.com.au/sports/football/${league}`, {
+		waitUntil: "networkidle0",
+	});
+
+	const teamAndOdds = await page.evaluate(() => {
+		const gamesList = [];
+
+		// All game cards
+		const gameCards = document.querySelectorAll(
+			".css-1a2d4ug-sportsStyles-styled-sportsStyles__SelectionsContainer-sportsStyles-styled.errtm9l18"
+		);
+
+		gameCards.forEach((game) => {
+			// All team names and odds within each card
+			const teams = game.querySelectorAll(
+				".css-ggkqa9-sportsStyles-styled-sportsStyles__OuterTitleContainer-sportsStyles-styled.errtm9l20"
+			);
+
+			const odds = game.querySelectorAll(
+				".errtm9l13.css-16r7ucc-Text-Text-sportsStyles-styled-sportsStyles__OddsText-sportsStyles-styled.ea6hjv30"
+			);
+
+			const gamesData = {
+				bookie: "BetDeluxe",
+				firstTeam: teams[0].innerText,
+				secondTeam: teams[2].innerText,
+				firstTeamOdds: odds[0].innerText,
+				drawOdds: odds[1].innerText,
+				secondTeamOdds: odds[2].innerText,
+			};
+
+			gamesList.push(gamesData);
+		});
+
+		return gamesList;
+	});
+
+	await browser.close();
+	return teamAndOdds;
+}
+
+module.exports = { betdeluxe, betdeluxeSoccer };
